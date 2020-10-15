d4d0cb4d9b3d4f289aef307577e76a87b25165b6
Mark Conroy <mark@mark.ie>
2020-10-15 22:02:21 +0100

    Adds a colours bit of fun

---
+++ colours/colours.js
@@ -0,0 +1,22 @@
+const baseURL = 'https://sampleapis.com/css-color-names/api/colors';
+const list = document.querySelector('.colour-list');
+
+fetch(baseURL)
+  .then(resp => resp.json())
+  .then(data => {
+    data
+    .sort(function (a, b) {
+      return a.name.localeCompare(b.name)
+    })
+    .forEach(datum => {
+      listItem = document.createElement('li');
+      listItem.classList.add('list-item');
+      listItem.innerHTML = `
+        <div class="list-item__panel" style="background-color: ${datum.hex}">${datum.name}</div>
+        <div class="list-item__hex">${datum.hex}</div>
+      `
+      list.appendChild(listItem);
+    });
+  });
+
+  
