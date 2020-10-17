ea0b8d4c3684f8947d977214a59153eb158b075c
Mark Conroy <mark@mark.ie>
2020-10-17 20:26:44 +0100

    add percent website colour item

---
+++ percent-website-colour/script.js
@@ -0,0 +1,15 @@
+function percentageOfTime() {
+  const numberOfSecondaInADay = 60 * 60 * 24;
+  const currentDate = new Date();
+  const hours = currentDate.getHours();
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+  const secondsSinceMidnight = (hours * 60 * 60) + (minutes * 60) + seconds;
+  // const percentTimePassed = Math.round(secondsSinceMidnight / numberOfSecondaInADay * 100);
+  const percentTimePassed = 40;
+  const rootStyle = document.documentElement.style;
+  rootStyle.setProperty('--percent-passed', percentTimePassed + '%')
+  percentTimePassed < 50 ? rootStyle.setProperty('--colour-secondary', '#fff') : rootStyle.setProperty('--colour-secondary', '#000');
+}
+
+percentageOfTime();
