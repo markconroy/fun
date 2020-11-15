8fd12cf4bb50d169f751af09ba6f53e6c78f469b
Mark Conroy <mark@mark.ie>
2020-11-15 19:52:44 +0000

    Basic example of font size set by number of articles

--- tagcloud/script.js
+++ tagcloud/script.js
@@ -1,19 +1,34 @@
+const dataURL = 'https://gist.githubusercontent.com/markconroy/ddb798678e17a918824644f17bca487c/raw/4c2e0d5ac57ff088937ab716e3ae36886c758dba/tags.json';
 const tags = document.querySelector(".tags");
 
+function handleFontSize(numberOfArticles) {
+  let fontSize = numberOfArticles * .5;
+  fontSize <= 1 ? fontSize = 1 : fontSize = fontSize;
+  fontSizeProperty = `${fontSize}em`;
+}
+
 function handleResult(result) {
-  const name = result.name;
-  const numberOfFilms = result.films.length;
+  // Set our variables
+  const name = result.title;
+  const numberOfArticles = result.tagged_articles.length;
+  const link = result.href;
+
+  // First, set the font-size depending on how many articles each tag has.
+  handleFontSize(numberOfArticles);
+  
+  // Then, create a list item for each tag and inline the font size.
   tag = document.createElement("li");
   tag.classList.add("tag");
-  tag.innerHTML = `${name} (${numberOfFilms})`;
+  tag.innerHTML = `<a href="${link}" style="font-size: ${fontSizeProperty}">${name} (${numberOfArticles})</a>`;
   tags.appendChild(tag);
 }
 
-fetch(`https://swapi.dev/api/people/`)
+fetch(dataURL)
   .then(function (res) {
     return res.json();
   })
   .then(function (data) {
-    const results = data.results;
-    results.forEach((result) => handleResult(result));
+    data.forEach((result) => handleResult(result));
   });
+
+
