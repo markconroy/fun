2d0169a904b7660fc19353faea217f84d6d34dfc
Mark Conroy <mark@mark.ie>
2020-11-19 19:01:01 +0000

    theming colours and more specific layout

--- tagcloud/script.js
+++ tagcloud/script.js
@@ -1,26 +1,33 @@
-const dataURL = 'https://gist.githubusercontent.com/markconroy/ddb798678e17a918824644f17bca487c/raw/4c2e0d5ac57ff088937ab716e3ae36886c758dba/tags.json';
+const dataURL =
+  "https://gist.githubusercontent.com/markconroy/ddb798678e17a918824644f17bca487c/raw/a4eeacd28fad81b0b828d0dda6ece2dcf193031b/tags.json";
 const tags = document.querySelector(".tags");
-
-function handleFontSize(numberOfArticles) {
-  let fontSize = numberOfArticles * .5;
-  fontSize <= 1 ? fontSize = 1 : fontSize = fontSize;
-  fontSizeProperty = `${fontSize}em`;
-}
 
 function handleResult(result) {
   // Set our variables
-  const name = result.title;
   const numberOfArticles = result.tagged_articles.length;
+  let fontSize = numberOfArticles * 1.25;
+  const name = result.title;
   const link = result.href;
-
-  // First, set the font-size depending on how many articles each tag has.
-  handleFontSize(numberOfArticles);
   
-  // Then, create a list item for each tag and inline the font size.
+  // First, set the font-size depending on how many articles each tag has.
+  if (fontSize <= 2) {
+    fontSize = 1;
+  } else if (fontSize >= 5) {
+    fontSize = 5;
+  } else {
+    fontSize = fontSize;
+  }
+  
+  const fontSizeProperty = `${fontSize}em`;
+
+  // Then, create a list element for each tag and inline the font size.
   tag = document.createElement("li");
   tag.classList.add("tag");
-  tag.innerHTML = `<a href="${link}" style="font-size: ${fontSizeProperty}">${name} (${numberOfArticles})</a>`;
+  tag.innerHTML = `<a class="tag__link" href="${link}" style="font-size: ${fontSizeProperty}">${name} (${numberOfArticles})</a>`;
+
+  // if (numberOfArticles > 1) {
   tags.appendChild(tag);
+  // }
 }
 
 fetch(dataURL)
@@ -30,5 +37,3 @@ fetch(dataURL)
   .then(function (data) {
     data.forEach((result) => handleResult(result));
   });
-
-
